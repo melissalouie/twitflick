3b64e547340d5df64b40797fc94e104a10bf6a45
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-03-03 13:54:06 -0700

    add field for tvshow form

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -2,4 +2,23 @@ class TvShowsController < ApplicationController
   def index
     @tvshows = TvShow.all
   end
+
+  def new
+    @tvshow = TvShow.new
+  end
+
+  def create
+    @tvshow = TvShow.new(tvshow_params)
+    if @tvshow.save
+      flash[:notice] = "TV Show successfully created."
+      redirect_to tv_show_path(@tvshow)
+    else
+      render :new
+    end
+  end
+
+  private
+  def tvshow_params
+    params.permit(:tv_show).require(:title, :genre)
+  end
 end
