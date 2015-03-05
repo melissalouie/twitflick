cd2d1230411422e108a45fa34d4b65a970788aaf
CaseyKelly <casey.kelly@colorado.edu>
2015-03-04 21:52:09 -0700

    Add validations and error messages for TV shows

--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -2,4 +2,6 @@ class TvShow < ActiveRecord::Base
   has_many :tv_show_reviews, dependent: :destroy
   has_many :users, through: :tvshowreviews
   belongs_to :genre
+  validates :title, presence: true, uniqueness: true
+  validates :genre_id, presence: true
 end
