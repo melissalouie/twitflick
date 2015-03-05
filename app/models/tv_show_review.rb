9df1dc62bda9d48cea7654074d479765faf53c6e
CaseyKelly <casey.kelly@colorado.edu>
2015-03-04 22:45:46 -0700

    Add TV show review validations, error messages, and limit ratings to 5 stars

--- app/models/tv_show_review.rb
+++ app/models/tv_show_review.rb
@@ -1,4 +1,12 @@
 class TvShowReview < ActiveRecord::Base
   belongs_to :tv_show
   belongs_to :user
+  validates :review, :rating, presence: true
+  validate :rating_less_than_five
+
+  def rating_less_than_five
+    if rating == nil || rating > 5 || rating < 1
+      errors.add(:ratings, "are from one to five stars")
+    end
+  end
 end
