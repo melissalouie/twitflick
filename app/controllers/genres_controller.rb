41ec0cc1df7f5b634aadf8c40c7a89b221301a65
Peter Vliet <petervliet@gmail.com>
2015-03-05 12:03:46 -0700

    all reviews for genre display on genre show page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,7 +2,7 @@ class GenresController < ApplicationController
 
   def show
     @genre = Genre.find(params[:id])
-    @tvshows = @genre.tv_shows
+    @tv_shows = @genre.tv_shows.sort_by(&:id).reverse
   end
 
 end
